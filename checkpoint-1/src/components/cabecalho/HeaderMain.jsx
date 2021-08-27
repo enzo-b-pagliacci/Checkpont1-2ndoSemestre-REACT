b12538afc019d35da19d1a5c14ac0bc5e57c83d6
enzo-pagliacci-fiap <RM87203@fiap.com.br>
2021-08-27 16:59:01 -0300

    botão toggler quase saindo

--- checkpoint-1/src/components/cabecalho/HeaderMain.jsx
+++ checkpoint-1/src/components/cabecalho/HeaderMain.jsx
@@ -1,30 +1,51 @@
 import React, {useState} from 'react'
 import logo from '../img/logoCP.PNG'
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+ 
+
 export default function HeaderMain() {
 
+   let visivel={
+      visibility: "visible" 
+   }
 
-   const [toggle, mostrarEsconder] = useState('hide')
+   const [toggle, mostrarEsconder] = useState(visivel)
+
+   const acionaToggler = ()=>{
+      visivel={
+         visibility: "hidden"
+      }
+      mostrarEsconder(visivel)
+   }
 
    return (
       <>
          <div className="header__main" >
             <nav className="content__menu">
-               <ul className="menu" >
-                  <img src={logo} alt="logo" className="logoCP"/>
-                  <button></button>
-                  <li><a href="#">Bussines</a></li>
-                  <li><a href="#">internacional</a></li>
-                  <li><a href="#">Saúde</a></li>
-                  <li><a href="#">Tecnologia</a></li>
-                  <li><a href="#">Esporte</a></li>
-                  <li><a href="#">Entreterimento</a></li>
-                  <li><a href="#">Estilo</a></li>
-                  <li><a href="#">Viagem & Gastronomia</a></li>
-                  <li><a href="#">Newstetters</a></li>
-                  <li><a href="#">Podcasts</a></li>
-               </ul>
+               <div className="menu" >
+                  <div className="logoEToggler">
+                     <div className="sectionToggler">
+                        <button onClick={()=> {acionaToggler()}} className="togglerMenu"><FontAwesomeIcon icon={faBars}  /></button>
+                     </div>
+                     <div className="sectionLogo">
+                        <img src={logo} alt="logo" className="logoCP"/>
+                     </div>
+                  </div>
+                  <ul className="menu" style={toggle}>
+                     <li><a href="#">Bussines</a></li>
+                     <li><a href="#">internacional</a></li>
+                     <li><a href="#">Saúde</a></li>
+                     <li><a href="#">Tecnologia</a></li>
+                     <li><a href="#">Esporte</a></li>
+                     <li><a href="#">Entreterimento</a></li>
+                     <li><a href="#">Estilo</a></li>
+                     <li><a href="#">Viagem & Gastronomia</a></li>
+                     <li><a href="#">Newstetters</a></li>
+                     <li><a href="#">Podcasts</a></li>
+                  </ul>
+               </div>
             </nav>
-
          </div>
 
       </>
