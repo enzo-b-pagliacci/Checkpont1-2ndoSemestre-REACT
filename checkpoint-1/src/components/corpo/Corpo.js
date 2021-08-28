9357d720096769f1f10064c3ed59e12a57789a5d
Jhoctan21 <rm88206@fiap.com.br>
2021-08-28 00:43:55 -0300

    Estiizando corpo

--- checkpoint-1/src/components/corpo/Corpo.js
+++ checkpoint-1/src/components/corpo/Corpo.js
@@ -5,20 +5,29 @@ export default function Corpo() {
 
    return(
       <>
+      <div className="row">
+        <div>1</div> 
+      </div>
       <div className="site_content">
-        <h1>Quais são os sintomas da variante Delta da Covid-19? Teste seus conhecimentos</h1>
+        <main className="posts.col__list">
+        <header className="post__header">
+        <h1 className="post__title" >Quais são os sintomas da variante Delta da Covid-19? Teste seus conhecimentos</h1>
+
         <hr></hr>
-        <p>A infecção causada pela variante Delta pode provocar sintomas diferentes da doença causada pela linhagem original do novo coronavírus</p>                   
+
+        <p className="post__excerpt">A infecção causada pela variante Delta pode provocar sintomas diferentes da doença causada pela linhagem original do novo coronavírus</p>                   
         <div>
           <img src={Author} alt="Rocha da CNN" />
         </div>
         <hr></hr>
         <p>24/08/2021 às 16:44 Atualizado às 16:56</p>
+        </header>
         <div>
         <img src={Logo} alt="CNN" className="logoCNN" />
         </div>
         <p>Por ser uma doença nova, a Covid-19 ainda não foi compreendida totalmente pela comunidade médica global. Os conhecimentos sobre a doença estão em constante atualização, incluindo os sintomas e os impactos para a saúde a curto e longo prazos.
         A variante Delta da Covid-19, originalmente conhecida como B.1.617.2, existe desde o final do ano passado, mas nos últimos meses tornou-se rapidamente dominante em muitos países. Ainda não está claro o quanto essa cepa é mais transmissível. Estimativas variam entre 60% e 200% mais transmissível, dependendo de quem apresenta a estimativa.</p>
+        </main>
 
       </div>
       
